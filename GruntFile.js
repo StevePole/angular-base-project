11d49e562f8055eb93e6877a4c5229d96fdf0569
Steve Pole <steve.pole@transferwise.com>
2015-06-18 14:21:13 +0100

    Delete banner setting.

--- GruntFile.js
+++ GruntFile.js
@@ -30,7 +30,6 @@ module.exports = function(grunt) {
         },
         uglify: {
             options: {
-                //banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
                 mangle: false,
                 beautify: true
             },
